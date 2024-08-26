20cbaf9be84aae9ec74e3d0e33b7e6f0b04ff67f
msotech <theusondev@gmail.com>
2024-08-26 16:53:53 -0300

    Criar modulo ToDo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField( max_length=120 )
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
--- todo_dellan/settings.py
+++ todo_dellan/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todo.urls'
+ROOT_URLCONF = 'todo_dellan.urls'
 
 TEMPLATES = [
     {
