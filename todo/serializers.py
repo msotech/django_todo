52252285c5c257b270479549acd0fc38572e0f0d
msotech <theusondev@gmail.com>
2024-08-26 18:57:01 -0300

    Correção do nome do serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from todo.models import ToDo
 
-class TodoSerializer(serializers.Modelserializer):
+class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
         fields = "__all__"
